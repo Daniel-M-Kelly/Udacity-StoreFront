3970390acda68fca8675b4b558a04cc52d51c62b
Daniel Kelly <email@danielmkelly.com>
2021-05-04 12:48:26 -0700

    feat: Add Database Migration

--- migrations/sqls/20210504175915-products-table-down.sql
+++ migrations/sqls/20210504175915-products-table-down.sql
@@ -1,1 +1,5 @@
 /* Replace with your SQL commands */
+DROP TABLE products;
+/* DROP TABLE users;
+DROP TABLE orders;
+DROP TABLE order_products; */
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
+//import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const EXPRESS_PORT = process.env.EXPRESS_PORT;
 
 const app: express.Application = express();
-const address = '0.0.0.0:3000';
+const address = `0.0.0.0:${EXPRESS_PORT}`;
 
-app.use(bodyParser.json());
+//bodyParser is deprecated, use express.json() instead
+//app.use(bodyParser.json());
+
+app.use(express.json());
 
 app.get('/', function (req: Request, res: Response) {
 	res.send('Hello World!');
